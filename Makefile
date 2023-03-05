d4c66bf866c4e36bf36511cc801f8bcf66693fff
COil <vernet.loic@gmail.com>
2023-03-05 19:22:53 +0100

    tests: added a E2E test example

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ SHELL = sh
 ## —— 🎶 The Symfony micro Makefile 🎶 —————————————————————————————————————————
 help: ## Outputs this help screen
 	@grep -E '(^[a-zA-Z0-9_-]+:.*?##.*$$)|(^##)' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}{printf "\033[32m%-30s\033[0m %s\n", $$1, $$2}' | sed -e 's/\[32m##/[33m/'
-.PHONY: help start stop test stan fix-php cs ci
+.PHONY: help start stop test coverage stan fix-php cs ci
 
 
 ## —— Symfony binary 💻 ————————————————————————————————————————————————————————
---
+++ tests/E2E/Controller/AppControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\E2E\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as E2ETestCase;
+
+// use Symfony\Component\Panther\PantherTestCase as E2ETestCase;
+
+/**
+ * Panther is not installed by default. To install it run "composer req --dev symfony/panther".
+ * Above, comment the first use and uncomment the second one. Same in the test,
+ * uncomment the first line and comment the second one.
+ *
+ * @see https://github.com/symfony/panther
+ */
+final class AppControllerTest extends E2ETestCase
+{
+    /**
+     * @see StaticControllerTest
+     */
+    public function testStimulus(): void
+    {
+        // $client = self::createPantherClient();
+        $client = self::createClient();
+        $client->request('GET', '/');
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('body', 'MicroSymfony');
+    }
+}
