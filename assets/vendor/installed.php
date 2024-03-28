169db1ac874e8bb6d1b6bec09c28469398b7f6c4
Florent Destremau <florent@windoo.fr>
2024-03-28 16:31:59 +0100

    Updated importmap

--- assets/vendor/installed.php
+++ assets/vendor/installed.php
@@ -9,16 +9,6 @@
     array (
     ),
   ),
-  'barecss' => 
-  array (
-    'version' => '1.1.1',
-    'dependencies' => 
-    array (
-    ),
-    'extraFiles' => 
-    array (
-    ),
-  ),
   'barecss/css/bare.min.css' => 
   array (
     'version' => '1.1.1',
--- importmap.php
+++ importmap.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Returns the importmap for this application.
  *
