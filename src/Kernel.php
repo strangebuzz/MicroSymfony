897657341baf4ea4cba2a924916c9ccc76375e7d
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-10-21 12:17:23 +0200

    Use test.service_container

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,21 +5,9 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function build(ContainerBuilder $container): void
-    {
-        $container->addCompilerPass(new class implements CompilerPassInterface {
-            public function process(ContainerBuilder $container): void
-            {
-                $container->getDefinition('twig')->setPublic(true);
-            }
-        });
-    }
 }
