4156921bcaa9228d23e3179210c23acffe910715
Loïc Vernet <vernet.loic@gmail.com>
2024-12-20 22:36:39 +0100

    fix: avoid error with Symfony 6.4 LTS downgrade (#139)

--- config/packages/csrf.php
+++ config/packages/csrf.php
@@ -3,8 +3,14 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Kernel;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    // This configutation file is specific to Symfony 7.2+
+    if (Kernel::VERSION_ID < 70200) {
+        return;
+    }
+
     // Enable stateless CSRF protection for forms and logins/logouts
     $containerConfigurator->extension('framework', [
         'form' => [
