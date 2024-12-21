7f37fdf1d8f0f02f37edf24c1a5a92814e6abc65
Loïc Vernet <vernet.loic@gmail.com>
2024-12-21 09:04:04 +0100

    feat: add the missing anchors on the website homepage + cleanup (#141)

---
+++ src/Twig/Extension/MarkdownExtension.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Extension;
+
+use App\Helper\StringHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+use function Symfony\Component\String\u;
+
+final class MarkdownExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly StringHelper $stringHelper,
+    ) {
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('add_headers_anchors', $this->addHeadersAnchors(...)),
+        ];
+    }
+
+    /**
+     * Add the missing anchors on demo website homepage displaying the Githb README.
+     *
+     * @see https://microsymfony.ovh
+     * @see https://github.com/strangebuzz/MicroSymfony/blob/main/README.md?plain=1
+     */
+    public function addHeadersAnchors(string $html): string
+    {
+        $dom = new \DOMDocument();
+        $dom->loadHTML(mb_encode_numericentity($html, [0x80, 0x10FFFF, 0, ~0], 'UTF-8'), \LIBXML_HTML_NOIMPLIED | \LIBXML_HTML_NODEFDTD);
+
+        /** @var \DOMNodeList<\DOMNode> $tags */
+        $tags = (new \DOMXPath($dom))->query('//h1 | //h2 | //h3 | //h4 | //h5 | //h6');
+        // Allow to have the same "buggy" anchors as GitHub
+        foreach ($tags as $headerTag) {
+            $slug = $this->stringHelper->slugify($headerTag->textContent);
+            /** @var \DOMElement $headerTag */
+            $headerTag->setAttribute('id', $slug.(u($slug)->length() !== u($headerTag->textContent)->length() ? '-' : '')); // add "-" when we have a final Emoji.
+        }
+
+        return (string) $dom->saveHTML();
+    }
+}
