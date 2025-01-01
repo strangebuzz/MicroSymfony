1464db77aa126d0e7324d06100c3db1c1363cd4d
COil <vernet.loic@gmail.com>
2025-01-01 19:29:45 +0100

    chore: fixed typo

--- config/packages/csrf.php
+++ config/packages/csrf.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symfony\Component\HttpKernel\Kernel;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    // This configutation file is specific to Symfony 7.2+
+    // This configuration file is specific to Symfony 7.2+
     if (Kernel::VERSION_ID < 70200) {
         return;
     }
