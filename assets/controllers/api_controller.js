277fb58d77baff40884cc5038a2ac08405a8b9b4
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-02-22 22:22:19 +0100

    apply formatter rules for js css

--- assets/controllers/api_controller.js
+++ assets/controllers/api_controller.js
@@ -1,27 +1,27 @@
-import {Controller} from '@hotwired/stimulus';
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ["title", "slug"]
+    static targets = ['title', 'slug'];
     static values = {
-        "url": String
-    }
+        url: String,
+    };
 
     connect() {
-        console.log("Dataset: ", this.element.dataset) // list all data properties of the controller
+        console.log('Dataset: ', this.element.dataset); // list all data properties of the controller
     }
 
     slugify() {
-        let apiUrl = this.urlValue + '?title=' + this.titleTarget.value
-        const slugTarget = this.slugTarget
+        let apiUrl = this.urlValue + '?title=' + this.titleTarget.value;
+        const slugTarget = this.slugTarget;
         fetch(apiUrl)
             .then(function (response) {
-                return response.json()
+                return response.json();
             })
             .then(function (data) {
-                slugTarget.value = data.slug
+                slugTarget.value = data.slug;
             })
             .catch(function (error) {
                 console.log('An error occured. 😞', error);
-            })
+            });
     }
 }
