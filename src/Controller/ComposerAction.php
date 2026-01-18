4fbdd642233056688901a05bd3e091591964cd85
COil <vernet.loic@gmail.com>
2026-01-18 06:52:51 +0100

    refactor: use Symfony to read files

--- src/Controller/ComposerAction.php
+++ src/Controller/ComposerAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
@@ -21,7 +22,7 @@ final class ComposerAction extends AbstractController
     #[Route(path: '/composer', name: self::class)]
     public function __invoke(): Response
     {
-        $composer = (string) file_get_contents(__DIR__.'/../../composer.json');
+        $composer = $fs->readFile(__DIR__.'/../../composer.json');
 
         return $this->render(self::class.'.html.twig', ['composer' => $composer]);
     }
--- src/Controller/HomeAction.php
+++ src/Controller/HomeAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
 // use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Attribute\Cache;
@@ -13,7 +14,8 @@ use Symfony\Component\Routing\Attribute\Route;
 /**
  * @see StaticRoutesSmokeTest
  */
-// using the AsController attribute is not mandatory when extending the Symfony AbstractController or when using the #Route attribute.
+// using the AsController attribute is not mandatory when extending the Symfony
+// AbstractController or when using the #Route attribute.
 // #[AsController]
 #[Cache(maxage: 3600, public: true)]
 final class HomeAction extends AbstractController
@@ -22,9 +24,9 @@ final class HomeAction extends AbstractController
      * Simple page with some content.
      */
     #[Route(path: '/', name: self::class)]
-    public function __invoke(): Response
+    public function __invoke(Filesystem $fs): Response
     {
-        $readme = (string) file_get_contents(__DIR__.'/../../README.md');
+        $readme = $fs->readFile(__DIR__.'/../../README.md');
 
         return $this->render(self::class.'.html.twig', ['readme' => $readme]);
     }
